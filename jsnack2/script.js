2507e5727a2ec4bbc615d74154fbe2159f9c0736
Ortello7 <domenicoortello00@gmail.com>
2024-12-03 12:23:20 +0100

    jsnack2

---
+++ jsnack2/script.js
@@ -0,0 +1,25 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+// Array contenente i nomi degli invitati
+const invitati = ["Domenico", "Ale", "Marco", "Richard", "Samu"];
+
+// Chiedi il nome all'utente
+const nomeUtente = prompt("Qual è il tuo nome?");
+
+// Variabile per determinare se l'utente è invitato
+let invitato = false;
+
+// Ciclo per controllare se il nome è presente nell'array
+for (let i = 0; i < invitati.length; i++) {
+    if (invitati[i].toLowerCase() === nomeUtente.toLowerCase()) {
+        invitato = true;
+        break;
+    }
+}
+
+// Comunica all'utente se è invitato o no
+if (invitato) {
+    alert("Puoi partecipare alla festa del Grande Gatsby!");
+} else {
+    alert("Mi dispiace, non sei nella lista degli invitati.");
+}
